8f8f55bddccdf71231a62f2ab99a65535292a333
Aaron-lab- c <aaron0321sy@gmail.com>
2019-10-25 14:50:43 +0800

    blog/settings.py add INSTALLED_APPS =[ 'main' ]
    main/views.py
    
    blog/urls.py add URL mapping

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogdb',
+        'NAME': 'bookstoredb',
         'USER': 'dbuser',
         'PASSWORD': 'dbuser',
         'HOST': 'localhost',
